5b14e6d738de26c9f78b9e67b50b4ba2fb518f7e
Shaun Kelly <shaunwilliamkelly@gmail.com>
2023-05-12 23:51:20 -0700

    updated JS toggle

--- script.js
+++ script.js
@@ -3,14 +3,21 @@
 const toggle = document.querySelector('.toggle');
 const links = document.querySelector('.links-list');
 const header = document.querySelector('header')
+const toggleIcon = document.querySelector('.material-symbols-outlined');
 
 toggle.addEventListener('click', function(){
     if(links.style.display === 'none'){
         links.style.display = 'block';
         toggle.style.backgroundColor = '#fff';
+        toggleIcon.style.color = "#000";
         
     } else{
         links.style.display = 'none';
-        toggle.style.backgroundColor = '#212121'
+        toggle.style.backgroundColor = '#212121';
+        toggleIcon.style.color = "#fff";
+       
     }
-})
+});
+
+
+// add income 
