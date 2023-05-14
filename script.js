3f56611a99818a6468cff237708825a4a85da46b
Shaun Kelly <shaunwilliamkelly@gmail.com>
2023-05-14 01:21:02 -0700

    organized everything and made classes that can be reused

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 // Toggle menu to show and hide links
 
 const toggle = document.querySelector('.toggle');
-const links = document.querySelector('.links-list');
-const header = document.querySelector('header')
-const toggleIcon = document.querySelector('.material-symbols-outlined');
 
-toggle.addEventListener('click', function(){
-    if(links.style.display === 'none'){
+toggle.addEventListener('click', function () {
+
+    const links = document.querySelector('.links-list');
+    const toggleIcon = document.querySelector('.material-symbols-outlined');
+
+    if (links.style.display === 'none') {
         links.style.display = 'block';
         toggle.style.backgroundColor = '#fff';
         toggleIcon.style.color = "#000";
-        
-    } else{
+
+    } else {
         links.style.display = 'none';
         toggle.style.backgroundColor = '#212121';
         toggleIcon.style.color = "#fff";
-       
+
     }
 });
 
 
 // add income 
+const addIncome = document.querySelector('.add-item-button');
+
+addIncome.addEventListener('click', function(){
+    const incomeTitleInput = document.getElementById('income-title-input');
+    const incomeAmountInput = document.getElementById('income-amount-input');
+
+    
+})
