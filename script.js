d652f95b14cce173d671cb671d3c0e425cf04b63
Shaun Kelly <shaunwilliamkelly@gmail.com>
2023-05-18 01:16:08 -0700

    removed budget JS because it needs to be reworked

--- script.js
+++ script.js
@@ -11,79 +11,79 @@ toggle.addEventListener('click', function (){
 });
 
 
-
+//Below needs to be reworked. Doesn't work on mobile
 
 // Add income and delete with total shown
-const incomeData = JSON.parse(localStorage.getItem('income')) || [];
-let total = 0;
+// const incomeData = JSON.parse(localStorage.getItem('income')) || [];
+// let total = 0;
 
-function renderIncomeList() {
-    const incomeList = document.getElementById('income-list');
-    incomeList.innerHTML = ''; // Clear the existing list
+// function renderIncomeList() {
+//     const incomeList = document.getElementById('income-list');
+//     incomeList.innerHTML = ''; // Clear the existing list
   
-    total = 0; // Reset the total amount
+//     total = 0; // Reset the total amount
   
-    incomeData.forEach((income, index) => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${income.title} - $${income.amount.toFixed(2)}`;
+//     incomeData.forEach((income, index) => {
+//       const listItem = document.createElement('li');
+//       listItem.textContent = `${income.title} - $${income.amount.toFixed(2)}`;
   
-      const deleteButton = document.createElement('button');
-      deleteButton.textContent = 'Delete';
-      deleteButton.addEventListener('click', () => {
-        deleteIncome(index);
-      });
+//       const deleteButton = document.createElement('button');
+//       deleteButton.textContent = 'Delete';
+//       deleteButton.addEventListener('click', () => {
+//         deleteIncome(index);
+//       });
   
-      listItem.appendChild(deleteButton);
-      incomeList.appendChild(listItem);
+//       listItem.appendChild(deleteButton);
+//       incomeList.appendChild(listItem);
   
-      total += income.amount;
-    });
+//       total += income.amount;
+//     });
   
-    // Update the total display
-    const totalDisplay = document.getElementById('income-amount-total');
-    totalDisplay.textContent = `Total: $${total.toFixed(2)}`;
-  }
+//     // Update the total display
+//     const totalDisplay = document.getElementById('income-amount-total');
+//     totalDisplay.textContent = `Total: $${total.toFixed(2)}`;
+//   }
 
-function deleteIncome(index) {
-  total -= incomeData[index].amount;
-  incomeData.splice(index, 1);
+// function deleteIncome(index) {
+//   total -= incomeData[index].amount;
+//   incomeData.splice(index, 1);
 
-  // Update the localStorage and re-render the income list
-  localStorage.setItem('income', JSON.stringify(incomeData));
-  renderIncomeList();
-}
+//   // Update the localStorage and re-render the income list
+//   localStorage.setItem('income', JSON.stringify(incomeData));
+//   renderIncomeList();
+// }
 
-renderIncomeList();
+// renderIncomeList();
 
-const incomeAddButton = document.getElementById('income-add-button');
-incomeAddButton.addEventListener('click', addIncome);
+// const incomeAddButton = document.getElementById('income-add-button');
+// incomeAddButton.addEventListener('click', addIncome);
 
-function addIncome() {
-  // Get the input values
-  const descriptionInput = document.getElementById('income-title-input');
-  const amountInput = document.getElementById('income-amount-input');
+// function addIncome() {
+//   // Get the input values
+//   const descriptionInput = document.getElementById('income-title-input');
+//   const amountInput = document.getElementById('income-amount-input');
 
-  const description = descriptionInput.value;
-  const amount = parseFloat(amountInput.value);
+//   const description = descriptionInput.value;
+//   const amount = parseFloat(amountInput.value);
 
-  if (description.trim() === '' || isNaN(amount)) {
-    alert('Please enter a valid description and amount.');
-    return;
-  }
+//   if (description.trim() === '' || isNaN(amount)) {
+//     alert('Please enter a valid description and amount.');
+//     return;
+//   }
 
-  // Clear the input fields
-  descriptionInput.value = '';
-  amountInput.value = '';
+//   // Clear the input fields
+//   descriptionInput.value = '';
+//   amountInput.value = '';
 
-  // Update the income array and total
-  const incomeInput = { title: description, amount };
-  incomeData.push(incomeInput);
-  total += amount;
+//   // Update the income array and total
+//   const incomeInput = { title: description, amount };
+//   incomeData.push(incomeInput);
+//   total += amount;
 
-  // Update the localStorage and re-render the income list
-  localStorage.setItem('income', JSON.stringify(incomeData));
-  renderIncomeList();
-}
+//   // Update the localStorage and re-render the income list
+//   localStorage.setItem('income', JSON.stringify(incomeData));
+//   renderIncomeList();
+// }
 
 
 
