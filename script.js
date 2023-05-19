9473d058de2bcbccc63c53c12990e35e2bbc4634
Shaun Kelly <rawtech@Shauns-iMac.lan>
2023-05-19 01:20:40 -0700

    JS added for planned amount section

--- script.js
+++ script.js
@@ -11,81 +11,38 @@ toggle.addEventListener('click', function (){
 });
 
 
-//Below needs to be reworked. Doesn't work on mobile
 
-// Add income and delete with total shown
-// const incomeData = JSON.parse(localStorage.getItem('income')) || [];
-// let total = 0;
-
-// function renderIncomeList() {
-//     const incomeList = document.getElementById('income-list');
-//     incomeList.innerHTML = ''; // Clear the existing list
-  
-//     total = 0; // Reset the total amount
-  
-//     incomeData.forEach((income, index) => {
-//       const listItem = document.createElement('li');
-//       listItem.textContent = `${income.title} - $${income.amount.toFixed(2)}`;
-  
-//       const deleteButton = document.createElement('button');
-//       deleteButton.textContent = 'Delete';
-//       deleteButton.addEventListener('click', () => {
-//         deleteIncome(index);
-//       });
-  
-//       listItem.appendChild(deleteButton);
-//       incomeList.appendChild(listItem);
-  
-//       total += income.amount;
-//     });
-  
-//     // Update the total display
-//     const totalDisplay = document.getElementById('income-amount-total');
-//     totalDisplay.textContent = `Total: $${total.toFixed(2)}`;
-//   }
-
-// function deleteIncome(index) {
-//   total -= incomeData[index].amount;
-//   incomeData.splice(index, 1);
-
-//   // Update the localStorage and re-render the income list
-//   localStorage.setItem('income', JSON.stringify(incomeData));
-//   renderIncomeList();
-// }
-
-// renderIncomeList();
-
-// const incomeAddButton = document.getElementById('income-add-button');
-// incomeAddButton.addEventListener('click', addIncome);
-
-// function addIncome() {
-//   // Get the input values
-//   const descriptionInput = document.getElementById('income-title-input');
-//   const amountInput = document.getElementById('income-amount-input');
-
-//   const description = descriptionInput.value;
-//   const amount = parseFloat(amountInput.value);
-
-//   if (description.trim() === '' || isNaN(amount)) {
-//     alert('Please enter a valid description and amount.');
-//     return;
-//   }
-
-//   // Clear the input fields
-//   descriptionInput.value = '';
-//   amountInput.value = '';
-
-//   // Update the income array and total
-//   const incomeInput = { title: description, amount };
-//   incomeData.push(incomeInput);
-//   total += amount;
-
-//   // Update the localStorage and re-render the income list
-//   localStorage.setItem('income', JSON.stringify(incomeData));
-//   renderIncomeList();
-// }
+//User input Planned Income
 
 
+const plannedAmount = document.getElementById('planned-amount-input');
 
+// Retrieve the stored value from localStorage on page load
+const storedValue = localStorage.getItem('plannedAmount');
+if (storedValue) {
+  displayText(storedValue);
+}
 
-// Use this to clear all storage if needed localStorage.removeItem('income'); 
+// Add event listener for the Enter key press
+plannedAmount.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    
+    
+    const userInput = plannedAmount.value
+    if (!isNaN(userInput)) {
+      displayText(userInput);
+
+    // Store the value in localStorage
+    localStorage.setItem('plannedAmount', userInput);
+    
+    } else {
+      alert('Invalid input. Please enter a number.');
+    }
+    plannedAmount.blur(); //to move focus out of the input section
+  }
+});
+
+function displayText(text) {
+ 
+  plannedAmount.value = `$${text}`;
+}
